8249cba32f7c70fe0f810fd2d6023adadc6ba594
FTCTutus1Tuus <ftc19168@gmail.com>
2026-01-05 21:13:52 -0600

    Made a statemachine for the ejection motor so i can be toggled

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/fsm/ShotgunFSM.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.team.fsm;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.team.DarienOpMode;
+
+public class ShotgunFSM {
+
+    private final DarienOpModeFSM opmode;
+    private final double SHOT_GUN_POWER_UP;
+    private final double SHOT_GUN_POWER_UP_FAR;
+    private final DcMotor shotgunMotor;
+
+    public enum State {
+        OFF,
+        POWER_UP,
+        POWER_UP_FAR;
+    }
+
+    private State current = State.OFF;
+
+    public ShotgunFSM(double powerlow, double powerhigh, DcMotor motor, DarienOpModeFSM opmode) {
+        this.SHOT_GUN_POWER_UP = powerlow;
+        this.SHOT_GUN_POWER_UP_FAR = powerhigh;
+        this.shotgunMotor = motor;
+        this.opmode = opmode;
+    }
+
+    /** Set the desired state explicitly. */
+    public void setState(State state) {
+        this.current = state;
+    }
+
+    /** Get the current state. */
+    public State getState() {
+        return current;
+    }
+
+    /** Convenience methods for transitions. */
+    public void toOff() {
+        current = State.OFF;
+        shotgunMotor.setPower(0);
+    }
+
+    public void toPowerUp() {
+        current = State.POWER_UP;
+        shotgunMotor.setPower(opmode.getVoltageAdjustedMotorPower(SHOT_GUN_POWER_UP));
+    }
+
+    public void toPowerUpFar() {
+        current = State.POWER_UP_FAR;
+        shotgunMotor.setPower(opmode.getVoltageAdjustedMotorPower(SHOT_GUN_POWER_UP_FAR));
+    }
+
+}
